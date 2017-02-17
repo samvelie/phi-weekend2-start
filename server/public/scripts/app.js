5bc45b643dce7016cc802e44094f8fe849f2cd7d
Kris Szafranski <kdszafranski@gmail.com>
2017-02-17 13:55:51 -0600

    updated json, commented client

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
+
+    // Upon page load, get the data from the server
     $.ajax({
       type: "GET",
       url: "/data",
       success: function(data){
+        // yay! we have data!
         console.log('returned data from server: ', data);
       }
     });
