cc67dcd99ab263f42226ba99ed7b229a73b335b6
samvelie <sam.velie@gmail.com>
2017-02-18 17:33:10 -0600

    ugly, not DRY, base mode functionality

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,42 +1,55 @@
 var indexOfPhier = 0;
-var phinalIndex = 18;
+var phinalIndex = 17;
 
 $(document).ready(function(){
 
   $.get('/data',function(data){
     phinalIndex = data.phirephiters.length - 1;
-    showInDOM(data.phirephiters[indexOfPhier].name);
+    showInDOM(data.phirephiters[indexOfPhier]);
+    for (var i = 0; i < data.phirephiters.length; i++) {
+      $('#trackingBar').append('<div class="marker"></div>');
+    }
+    $('.marker:eq(' + indexOfPhier + ')').css({'background-color': 'blue', 'border-top': '1px solid red'});
   });
 
   $('#next').on('click', function(){
-    if(indexOfPhier==17){
+    if(indexOfPhier==phinalIndex){
       indexOfPhier=0;
+      $('.marker:eq(' + phinalIndex +')').css({'background-color': 'gray', 'border-top': '1px solid blue'});
     } else{
     indexOfPhier++;
     }
 
     $.get('/data',function(data){
-      showInDOM(data.phirephiters[indexOfPhier].name);
+      showInDOM(data.phirephiters[indexOfPhier]);
     })
 
+    $('.marker:eq(' + indexOfPhier + ')').css({'background-color': 'blue', 'border-top': '1px solid red'});
+    $('.marker:eq(' + indexOfPhier + ')').prev().css({'background-color': 'gray', 'border-top': '1px solid blue'});
   });
 
   $('#previous').on('click', function(){
     if(indexOfPhier==0){
-      indexOfPhier=17;
+      indexOfPhier=phinalIndex;
+      $('.marker:eq(0)').css({'background-color': 'gray', 'border-top': '1px solid blue'});
     } else{
     indexOfPhier--;
     }
 
     $.get('/data',function(data){
-      showInDOM(data.phirephiters[indexOfPhier].name);
+      showInDOM(data.phirephiters[indexOfPhier]);
+      console.log(data);
     })
 
+    $('.marker:eq(' + indexOfPhier + ')').css({'background-color': 'blue', 'border-top': '1px solid red'});
+    $('.marker:eq(' + indexOfPhier + ')').next().css({'background-color': 'gray', 'border-top': '1px solid blue'});
   });
 
 
     function showInDOM(person){
-      $('#people').text(person);
+      $('#people').find('h1').text(person.name);
+      $('#people').find('h2').text('https://github.com/' + person.git_username);
+      $('#people').find('h3').text('"'+ person.shoutout + '"');
     }
 });
 
