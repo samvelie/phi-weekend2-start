1065f550fdf0f0b26dea1c53bba8065aa6aade94
samvelie <sam.velie@gmail.com>
2017-02-18 15:28:02 -0600

    basic forward and backward use  working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,13 @@
+var indexOfPhier = 0;
+
 $(document).ready(function(){
 
+  $('#next').on('click', function(){
+    if(indexOfPhier==17){
+      indexOfPhier=0;
+    } else{
+    indexOfPhier++;
+    }
     // Upon page load, get the data from the server
     $.ajax({
       type: "GET",
@@ -7,6 +15,32 @@ $(document).ready(function(){
       success: function(data){
         // yay! we have data!
         console.log('returned data from server: ', data);
+        showInDOM(data.phirephiters[indexOfPhier].name)
       }
     });
+  })
+
+  $('#previous').on('click', function(){
+    if(indexOfPhier==0){
+      indexOfPhier=17;
+    } else{
+    indexOfPhier--;
+    }
+    // Upon page load, get the data from the server
+    $.ajax({
+      type: "GET",
+      url: "/data",
+      success: function(data){
+        // yay! we have data!
+        console.log('returned data from server: ', data);
+        showInDOM(data.phirephiters[indexOfPhier].name)
+      }
+    });
+  })
+
+
+
+    function showInDOM(person){
+      $('#people').text(person);
+    }
 });
